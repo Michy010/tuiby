bb56bd49f094809fed9f8c1e475654c36ae2d4fe
Michy010 <michaelmsita17@gmail.com>
2025-05-10 16:52:17 +0300

    new deployments

--- tuiby/settings.py
+++ tuiby/settings.py
@@ -92,12 +92,15 @@ WSGI_APPLICATION = 'tuiby.wsgi.application'
 import dj_database_url
 import os
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL'),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.parse(env('DATABASE_URL', default='sqlite:///db.sqlite3'))
 }
 
 
